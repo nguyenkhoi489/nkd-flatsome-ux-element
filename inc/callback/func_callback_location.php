768c0ae0ffa47cf6b3a9234a82ed460b5dece221
Nguyen Khoi PC <ngnguyenkhoib4@gmail.com>
2024-07-09 22:10:09 +0700

    remove label for func_callback_location

--- inc/callback/func_callback_location.php
+++ inc/callback/func_callback_location.php
@@ -4,7 +4,6 @@ function func_callback_location($object)
 {
 ?>
     <div class="form-group">
-        <label for="locationAddress">Location address</label>
         <input type="text" name="locationAddress" class="form-control" id="locationAddress" placeholder="Hồ Chí Minh">
     </div>
 <?php
