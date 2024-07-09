9fdbb747d160dac32f35fbcb435700fe4a829478
Nguyen Khoi PC <ngnguyenkhoib4@gmail.com>
2024-07-09 22:03:45 +0700

    render content for callback_func_location

--- inc/callback/func_callback_location.php
+++ inc/callback/func_callback_location.php
@@ -2,5 +2,10 @@
 
 function func_callback_location($object)
 {
-
+?>
+    <div class="form-group">
+        <label for="exampleFormControlInput1">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+<?php
 }
