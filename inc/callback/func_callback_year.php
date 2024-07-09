b2b4b4393ee74e1cffb78e4c8c23aadb7ec6a330
Nguyen Khoi PC <ngnguyenkhoib4@gmail.com>
2024-07-09 22:07:59 +0700

    render content for func_callback_year

--- inc/callback/func_callback_year.php
+++ inc/callback/func_callback_year.php
@@ -2,5 +2,9 @@
 
 function func_callback_year($object)
 {
-
+    ?>
+    <div class="form-group">
+        <input type="text" name="year" class="form-control" id="year" placeholder="2024">
+    </div>
+<?php
 }
